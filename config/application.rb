4a920d95368504a9d0b132a1ee72b242f139a0fa
Drew Bomhof <dbomhof@redhat.com>
2018-08-14 17:48:02 -0400

    Fixed configuration to allow frontend to run
    Added the rack-cors gem
    Added entry for the middleware to look for the right incoming url

--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,12 @@ module ApiDemo
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins 'https://prod.foo.redhat.com:1337'
+        resource '*', headers: :any, methods: [:get, :post, :options]
+      end
+    end
   end
 end
