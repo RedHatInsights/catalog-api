1450db7bb67459cdd32c587abd99149c962c1d55
Drew Bomhof <dbomhof@redhat.com>
2018-08-27 17:05:47 -0400

    Rework how Dev Docker file and supporting files are managed

--- config/containers/app_cmd.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-exec bundle exec /var/www/svccat/bin/rails server -b 'ssl://0.0.0.0:5000?key=/var/www/svccat/config/ssl/localhost.key&cert=/var/www/svccat/config/ssl/localhost.crt' 
-
