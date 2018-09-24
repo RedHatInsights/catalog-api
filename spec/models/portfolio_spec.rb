751de009b823b2f74764bbc97b7b3b8717636d5a
Drew Bomhof <dbomhof@redhat.com>
2018-09-24 16:27:16 -0400

    Consolidate rails_helper into spec_helper

--- spec/models/portfolio_spec.rb
+++ spec/models/portfolio_spec.rb
@@ -1,8 +1,8 @@
 describe Portfolio do
-  let(:portfolio)       { FactoryBot.create(:portfolio) }
-  let(:portfolio_item)  { FactoryBot.create(:portfolio_item) }
+  let(:portfolio)      { create(:portfolio) }
+  let(:portfolio_item) { create(:portfolio_item) }
 
-  context "#add_portfolio_item" do
+  describe "#add_portfolio_item" do
     it "returns the last added portfolio_item" do
       expect(portfolio.add_portfolio_item(portfolio_item.id).first).to be_a PortfolioItem
     end
