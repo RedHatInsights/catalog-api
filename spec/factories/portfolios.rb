d8f7dc4e018db43d04aa86e482f2d6cef32f73c3
Drew Bomhof <dbomhof@redhat.com>
2018-10-10 15:50:15 -0400

    Add specs validating tenancy and testing the routes

--- spec/factories/portfolios.rb
+++ spec/factories/portfolios.rb
@@ -2,5 +2,10 @@ FactoryBot.define do
   factory :portfolio do
     sequence(:name)         { |n| "Portfolio_name_#{n}" }
     sequence(:description)  { |n| "Portfolio_description_#{n}" }
+
+    tenant
+    trait :without_tenant do
+      tenant_id { nil }
+    end
   end
 end
--- spec/factories/tenants.rb
+++ spec/factories/tenants.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :tenant do
     sequence(:ref_id)         { |n| "#{n}" }
+
+    trait :with_external_tenant do
+      ref_id { '111' }
+    end
   end
 end
