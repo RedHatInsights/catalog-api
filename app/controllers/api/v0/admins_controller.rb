3dd26eb3e6a7af13a37be6d4576b5bb143125dfa
Drew Bomhof <dbomhof@redhat.com>
2018-12-19 13:45:41 -0500

    remove rescue and allow rails to handle exceptions natively

--- app/controllers/api/v0/admins_controller.rb
+++ app/controllers/api/v0/admins_controller.rb
@@ -13,8 +13,6 @@ module Api
         portfolio.update!(portfolio_params)
 
         render :json => portfolio
-      rescue ActiveRecord::RecordInvalid => e
-        render :json => { :errors => e.message }, :status => :unprocessable_entity
       end
 
       def add_portfolio_item_to_portfolio
