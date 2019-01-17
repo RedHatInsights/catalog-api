fe7b581cb2ade79cf7928adabeb98ec0ffad4f94
Madhu Kanoor <mkanoor@redhat.com>
2019-01-17 17:48:27 -0500

    Fixed rubocop warnings

--- spec/factories/portfolio_items.rb
+++ spec/factories/portfolio_items.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :portfolio_item do
     sequence(:name)                 { |n| "PortfolioItem_name_#{n}" }
     sequence(:description)          { |n| "PortfolioItem_description_#{n}" }
-    sequence(:service_offering_ref) { |n| "#{rand(0)+n}" }
-    sequence(:service_offering_source_ref) { |n| "#{rand(0)+n}" }
+    sequence(:service_offering_ref) { |n| (rand(0) + n).to_s }
+    sequence(:service_offering_source_ref) { |n| (rand(0) + n).to_s }
   end
 end
