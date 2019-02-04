1583f12053fe14f45663c0b7d123d2ceae8cc9b9
Madhu Kanoor <mkanoor@redhat.com>
2019-02-04 15:12:58 -0500

    Don't eager load code on boot

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,11 +4,15 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
 
-  # Eager load code on boot. This eager loads most of Rails and
+  # Don't eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
   # Rake tasks automatically ignore this option for performance.
-  config.eager_load = true
+
+  # Enabling eagerloading also disables autoloading which causes problems
+  # in production that don't happen in development.
+  config.eager_load_paths = []
+  config.eager_load = false
 
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
