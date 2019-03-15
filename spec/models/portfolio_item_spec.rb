a881b434edf4f48fb91cb30e008528d74b5399af
Jacob Lindgren <jlindgre@redhat.com>
2019-03-15 16:18:16 -0500

    Add portfolio_item#resolved_workflow_ref and tests

--- spec/models/portfolio_item_spec.rb
+++ spec/models/portfolio_item_spec.rb
@@ -32,4 +32,46 @@ describe PortfolioItem do
       expect(item).to be_valid
     end
   end
+
+  context "#item_workflow_ref" do
+    let(:item_workflow_ref) { "portfolio_item_workflow_ref" }
+    let(:portfolio_workflow_ref) { "portfolio_workflow_ref" }
+    let(:portfolio) { create(:portfolio, :workflow_ref => portfolio_workflow_ref) }
+
+    let(:portfolio_item) do
+      create(:portfolio_item,
+             :service_offering_ref => "123",
+             :portfolio_id         => portfolio.id,
+             :workflow_ref         => item_workflow_ref)
+    end
+
+    let(:ref) { portfolio_item.send(:item_workflow_ref) }
+
+    context "when the portfolio_item has a workflow_ref" do
+      it "returns the portfolio_item's workflow ref" do
+        expect(ref).to eq item_workflow_ref
+      end
+    end
+
+    context "when the portfolio_item is missing the workflow ref" do
+      before do
+        portfolio_item.update(:workflow_ref => nil)
+      end
+
+      it "looks up the workflow_ref on the portfolio" do
+        expect(ref).to eq portfolio_workflow_ref
+      end
+    end
+
+    context "when niether the portfolio item nor portfolio have a workflow_ref" do
+      before do
+        portfolio_item.update(:workflow_ref => nil)
+        portfolio.update(:workflow_ref => nil)
+      end
+
+      it "returns nil without throwing an error" do
+        expect(ref).to be_nil
+      end
+    end
+  end
 end
