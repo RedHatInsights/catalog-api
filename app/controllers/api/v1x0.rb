98b335e34c3932caccd7bf54ec839d429379fdd8
Drew Bomhof <dbomhof@redhat.com>
2019-03-28 19:36:01 -0400

    Allow only v1.0.0 for openapi.json

--- app/controllers/api/v1x0.rb
+++ app/controllers/api/v1x0.rb
@@ -2,7 +2,7 @@ module Api
   module V1x0
     class RootController < ApplicationController
       def openapi
-        render :json => Rails.root.join('public', 'catalog', 'v0.1.0', 'openapi.json').read
+        render :json => Rails.root.join('public', 'catalog', 'v1.0.0', 'openapi.json').read
       end
     end
   end
