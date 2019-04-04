4c45c1ca384ac49601624e0a7dc5043a53131360
Drew Bomhof <dbomhof@redhat.com>
2019-04-04 16:32:08 -0400

    Remove admin, user tagged tests

--- spec/requests/portfolios_spec.rb
+++ spec/requests/portfolios_spec.rb
@@ -13,102 +13,98 @@ describe 'Portfolios API' do
   let(:new_portfolio_item_id) { portfolio_item.id }
   let(:tenant)                { create(:tenant, :with_external_tenant) }
 
-  %w(admin user).each do |tag|
-    describe "GET #{tag} tagged /portfolios/:portfolio_id" do
-      before do
-        get "#{api}/portfolios/#{portfolio_id}", :headers => send("#{tag}_headers")
-      end
-
-      context 'when portfolios exist' do
-        it 'returns status code 200' do
-          expect(response).to have_http_status(200)
-        end
-
-        it 'returns portfolio requested' do
-          expect(json).not_to be_empty
-          expect(json['id']).to eq(portfolio_id.to_s)
-          expect(json['created_at']).to eq(portfolio.created_at.iso8601)
-        end
-      end
+  describe "GET /portfolios/:portfolio_id" do
+    before do
+      get "#{api}/portfolios/#{portfolio_id}", :headers => default_headers
     end
 
-    describe "GET #{tag} v1.0 tagged /portfolios/:portfolio_id" do
-      before do
-        get "#{api}/portfolios/#{portfolio_id}", :headers => send("#{tag}_headers")
-      end
-
-      context 'when portfolios exist' do
-        it 'returns status code 200' do
-          expect(response).to have_http_status(200)
-        end
-
-        it 'returns portfolio requested' do
-          expect(json).not_to be_empty
-          expect(json['id']).to eq(portfolio_id.to_s)
-        end
-      end
-    end
-
-    describe "GET #{tag} tagged /portfolios/:portfolio_id/portfolio_items" do
-      before do
-        get "#{api}/portfolios/#{portfolio_id}/portfolio_items", :headers => send("#{tag}_headers")
-      end
-
-      it 'returns all associated portfolio_items' do
-        expect(json).not_to be_empty
-        expect(json['meta']['count']).to eq 1
-        portfolio_item_ids = portfolio_items.map { |x| x.id.to_s }.sort
-        expect(json['data'].map { |x| x['id'] }.sort).to eq portfolio_item_ids
-      end
-    end
-
-    describe "GET #{tag} tagged /portfolios_items/:portfolio_item_id" do
-      before do
-        get "#{api}/portfolio_items/#{portfolio_item_id}", :headers => send("#{tag}_headers")
-      end
-
-      it 'returns an associated portfolio_item for a specific portfolio' do
-        expect(json).not_to be_empty
-        expect(json['id']).to eq portfolio_item_id.to_s
-      end
-    end
-
-    describe "POST admin tagged /portfolios/:portfolio_id/portfolio_items" do
-      #let(:portfolio) { create(:portfolio) }
-      #let(:portfolio_item) { create(:portfolio_item) }
-      let(:params) { {:portfolio_item_id => portfolio_item.id} }
-      before do
-        post "#{api}/portfolios/#{portfolio.id}/portfolio_items", :params => params, :headers => send("#{tag}_headers")
-      end
-
-      it 'returns a 200' do
+    context 'when portfolios exist' do
+      it 'returns status code 200' do
         expect(response).to have_http_status(200)
       end
 
-      it 'returns the portfolio_item which now points back to the portfolio' do
-        expect(json.size).to eq 1
-        expect(json.first['portfolio_id']).to eq portfolio.id.to_s
-      end
-    end
-
-    describe "GET #{tag} tagged /portfolios" do
-      before do
-        get "#{api}/portfolios", :headers => send("#{tag}_headers")
-      end
-
-      context 'when portfolios exist' do
-        it 'returns status code 200' do
-          expect(response).to have_http_status(200)
-        end
-
-        it 'returns all portfolio requests' do
-          expect(json['data'].size).to eq(1)
-        end
+      it 'returns portfolio requested' do
+        expect(json).not_to be_empty
+        expect(json['id']).to eq(portfolio_id.to_s)
+        expect(json['created_at']).to eq(portfolio.created_at.iso8601)
       end
     end
   end
 
-  describe 'admin tagged /portfolios', :type => :routing do
+  describe "GET v1.0 /portfolios/:portfolio_id" do
+    before do
+      get "#{api}/portfolios/#{portfolio_id}", :headers => default_headers
+    end
+
+    context 'when portfolios exist' do
+      it 'returns status code 200' do
+        expect(response).to have_http_status(200)
+      end
+
+      it 'returns portfolio requested' do
+        expect(json).not_to be_empty
+        expect(json['id']).to eq(portfolio_id.to_s)
+      end
+    end
+  end
+
+  describe "GET /portfolios/:portfolio_id/portfolio_items" do
+    before do
+      get "#{api}/portfolios/#{portfolio_id}/portfolio_items", :headers => default_headers
+    end
+
+    it 'returns all associated portfolio_items' do
+      expect(json).not_to be_empty
+      expect(json['meta']['count']).to eq 1
+      portfolio_item_ids = portfolio_items.map { |x| x.id.to_s }.sort
+      expect(json['data'].map { |x| x['id'] }.sort).to eq portfolio_item_ids
+    end
+  end
+
+  describe "GET /portfolios_items/:portfolio_item_id" do
+    before do
+      get "#{api}/portfolio_items/#{portfolio_item_id}", :headers => default_headers
+    end
+
+    it 'returns an associated portfolio_item for a specific portfolio' do
+      expect(json).not_to be_empty
+      expect(json['id']).to eq portfolio_item_id.to_s
+    end
+  end
+
+  describe "POST /portfolios/:portfolio_id/portfolio_items" do
+    let(:params) { {:portfolio_item_id => portfolio_item.id} }
+    before do
+      post "#{api}/portfolios/#{portfolio.id}/portfolio_items", :params => params, :headers => default_headers
+    end
+
+    it 'returns a 200' do
+      expect(response).to have_http_status(200)
+    end
+
+    it 'returns the portfolio_item which now points back to the portfolio' do
+      expect(json.size).to eq 1
+      expect(json.first['portfolio_id']).to eq portfolio.id.to_s
+    end
+  end
+
+  describe "GET /portfolios" do
+    before do
+      get "#{api}/portfolios", :headers => default_headers
+    end
+
+    context 'when portfolios exist' do
+      it 'returns status code 200' do
+        expect(response).to have_http_status(200)
+      end
+
+      it 'returns all portfolio requests' do
+        expect(json['data'].size).to eq(1)
+      end
+    end
+  end
+
+  describe '/portfolios', :type => :routing do
     let(:valid_attributes) { { :name => 'rspec 1', :description => 'rspec 1 description' } }
     context 'with wrong header' do
       it 'returns a 404' do
@@ -117,7 +113,7 @@ describe 'Portfolios API' do
     end
   end
 
-  describe 'DELETE admin tagged /portfolios/:portfolio_id' do
+  describe 'DELETE /portfolios/:portfolio_id' do
     let(:valid_attributes) { { :name => 'PatchPortfolio', :description => 'description for patched portfolio' } }
 
     context 'when discarding a portfolio' do
@@ -145,7 +141,7 @@ describe 'Portfolios API' do
     end
   end
 
-  describe 'PATCH admin tagged /portfolios/:portfolio_id' do
+  describe 'PATCH /portfolios/:portfolio_id' do
     let(:valid_attributes) { { :name => 'PatchPortfolio', :description => 'description for patched portfolio', :workflow_ref => "123456" } }
     let(:invalid_attributes) { { :fred => 'nope', :bob => 'bob portfolio' } }
     context 'when patched portfolio is valid' do
@@ -181,7 +177,7 @@ describe 'Portfolios API' do
     end
   end
 
-  describe 'POST admin tagged /portfolios' do
+  describe 'POST /portfolios' do
     let(:valid_attributes) { { :name => 'rspec 1', :description => 'rspec 1 description' } }
     context 'when portfolio attributes are valid' do
       before { post "#{api}/portfolios", :params => valid_attributes, :headers => admin_headers }
