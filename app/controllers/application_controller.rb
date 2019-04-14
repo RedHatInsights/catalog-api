33b9a52bab5b24c323853f88aa94c0266364c215
Madhu Kanoor <mkanoor@redhat.com>
2019-04-13 20:58:31 -0400

    Handle IdentityError
    Fixes #233

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,8 @@
 class ApplicationController < ActionController::API
   include Response
   rescue_from Catalog::TopologyError, :with => :topology_service_error
-  rescue_from Catalog::NotAuthorized, :with => :not_authorized_error
+  rescue_from Catalog::NotAuthorized, :with => :forbidden_error
+  rescue_from ManageIQ::API::Common::IdentityError, :with => :unauthorized_error
 
   around_action :with_current_request
 
@@ -34,7 +35,11 @@ class ApplicationController < ActionController::API
     render :json => {:message => err.message}, :status => :internal_server_error
   end
 
-  def not_authorized_error(err)
+  def forbidden_error(err)
     render :json => {:message => err.message}, :status => :forbidden
+  end
+
+  def unauthorized_error(err)
+    render :json => {:message => err.message}, :status => :unauthorized
   end
 end
