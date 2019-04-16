f8410008218ec6dae1d1768b56ec871be597c1b0
Drew Bomhof <dbomhof@redhat.com>
2019-04-16 18:05:22 -0400

    RE Re enable RBAC

--- app/controllers/api/v1x0/mixins/rbac_mixin.rb
+++ app/controllers/api/v1x0/mixins/rbac_mixin.rb
@@ -3,9 +3,9 @@ module Api
     module Mixins
       module RBACMixin
         def write_access_check
-          return #unless RBAC::Access.enabled?
-          #access_obj = RBAC::Access.new(controller_name.classify.constantize.table_name, 'write').process
-          #raise Catalog::NotAuthorized, "Write access not authorized for #{controller_name.classify.constantize}" unless access_obj.accessible?
+        return unless RBAC::Access.enabled?
+        access_obj = RBAC::Access.new(controller_name.classify.constantize.table_name, 'write').process
+        raise Catalog::NotAuthorized, "Write access not authorized for #{controller_name.classify.constantize}" unless access_obj.accessible?
         end
       end
     end
