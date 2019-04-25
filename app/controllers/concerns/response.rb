8bca552ab57b5964b445ec7475b885a833aac76d
Jacob Lindgren <jlindgre@redhat.com>
2019-04-25 16:28:53 -0500

    Refactor move ApplicationController#*_error methods to Response mixin

--- app/controllers/concerns/response.rb
+++ app/controllers/concerns/response.rb
@@ -2,4 +2,18 @@ module Response
   def json_response(object, status = :ok)
     render :json => object, :status => status
   end
+
+  def topology_service_error(err)
+    render :json => {:message => err.message}, :status => :internal_server_error
+  end
+
+  def forbidden_error(err)
+    Rails.logger.error("Forbidden error: #{err.message}")
+    render :json => {:message => "Forbidden"}, :status => :forbidden
+  end
+
+  def unauthorized_error(err)
+    Rails.logger.error("Unauthorized error: #{err.message}")
+    render :json => {:message => "Unauthorized"}, :status => :unauthorized
+  end
 end
