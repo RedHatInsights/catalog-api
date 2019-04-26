5eda863d1b1b2c5876ce107a46c6652cec2f944c
Drew Bomhof <dbomhof@redhat.com>
2019-04-26 15:43:26 -0400

    Remove potential duplication from sequence
    This code was not right from the beginning. It should have
    been something like { |n| #{n}12345 }
    adding in the rand() actually guaranteed we would have duplicates

--- spec/factories/tenants.rb
+++ spec/factories/tenants.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :tenant do
-    sequence(:external_tenant) { |n| n + rand(1000) }
+    sequence(:external_tenant)
 
     trait :with_external_tenant do
       external_tenant { "0369233" }
