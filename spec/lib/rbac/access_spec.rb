7d9a47ad10d65bda4f37ce3c087633e7dfa72c6e
Madhu Kanoor <mkanoor@redhat.com>
2019-04-30 14:52:28 -0400

    Use instance_double for RBAC test objects

--- spec/lib/rbac/access_spec.rb
+++ spec/lib/rbac/access_spec.rb
@@ -1,22 +1,7 @@
 describe RBAC::Access do
-  let(:app_name) { 'catalog' }
-  let(:resource) { "portfolio" }
-  let(:verb) { "read" }
-  let(:attr_filter1) { double(:key => 'id', :operation => 'equal', :value => '25') }
-  let(:attr_filter2) { double(:key => 'id', :operation => 'equal', :value => '26') }
-  let(:attr_filter3) { double(:key => 'id', :operation => 'equal', :value => '27') }
-  let(:attr_filter4) { double(:key => 'id', :operation => 'equal', :value => '*') }
-  let(:resource_def1) { double(:attribute_filter => attr_filter1) }
-  let(:resource_def2) { double(:attribute_filter => attr_filter2) }
-  let(:resource_def3) { double(:attribute_filter => attr_filter3) }
-  let(:resource_def4) { double(:attribute_filter => attr_filter4) }
-  let(:access1) { double(:permission => "#{app_name}:#{resource}:read", :resource_definitions => [resource_def1, resource_def3]) }
-  let(:access2) { double(:permission => "#{app_name}:#{resource}:write", :resource_definitions => [resource_def2]) }
-  let(:access3) { double(:permission => "#{app_name}:#{resource}:order", :resource_definitions => []) }
-  let(:access4) { double(:permission => "#{app_name}:#{resource}:read", :resource_definitions => [resource_def4]) }
-  let(:acls) { [access1, access2, access3] }
-  let(:all_access_acls) { [access4] }
+  include_context "rbac_objects"
 
+  let(:verb) { "read" }
   let(:rbac_access) { described_class.new(resource, verb) }
   let(:api_instance) { double }
   let(:rs_class) { class_double("RBAC::Service").as_stubbed_const(:transfer_nested_constants => true) }
@@ -27,17 +12,17 @@ describe RBAC::Access do
 
   it "fetches the array of plans" do
     with_modified_env :APP_NAME => app_name do
-      allow(RBAC::Service).to receive(:paginate).with(api_instance, :get_principal_access, {}, app_name).and_return(acls)
+      allow(RBAC::Service).to receive(:paginate).with(api_instance, :get_principal_access, {}, app_name).and_return([access1])
       svc_obj = rbac_access.process
       expect(svc_obj.acl.count).to eq(1)
       expect(svc_obj.accessible?).to be_truthy
-      expect(svc_obj.id_list).to match_array(%w[25 27])
+      expect(svc_obj.id_list).to match_array([resource_id1])
     end
   end
 
   it "* in id gives access to all instances" do
     with_modified_env :APP_NAME => app_name do
-      allow(RBAC::Service).to receive(:paginate).with(api_instance, :get_principal_access, {}, app_name).and_return(all_access_acls)
+      allow(RBAC::Service).to receive(:paginate).with(api_instance, :get_principal_access, {}, app_name).and_return([admin_access])
       svc_obj = rbac_access.process
       expect(svc_obj.acl.count).to eq(1)
       expect(svc_obj.accessible?).to be_truthy
--- spec/support/rbac_shared_contexts.rb
+++ spec/support/rbac_shared_contexts.rb
@@ -5,32 +5,40 @@ RSpec.shared_context "rbac_objects" do
   let(:resource_id1) { "1" }
   let(:resource_id2) { "2" }
   let(:resource_id3) { "3" }
-  let(:group1) { double(:name => 'group1', :uuid => "123") }
-  let(:group2) { double(:name => 'group2', :uuid => "12345") }
-  let(:group3) { double(:name => 'group3', :uuid => "45665") }
-  let(:role1) { double(:name => "#{app_name}-#{resource}-#{resource_id1}-group-#{group1.uuid}", :uuid => "67899") }
-  let(:role2) { double(:name => "#{app_name}-#{resource}-#{resource_id2}-group-#{group1.uuid}", :uuid => "55555") }
-  let(:role1_detail) { double(:name => role1.name, :uuid => role1.uuid, :access => [access1]) }
-  let(:role2_detail) { double(:name => role2.name, :uuid => role2.uuid, :access => []) }
-  let(:role1_detail_updated) { double(:name => role1.name, :uuid => role1.uuid, :access => []) }
+  let(:group1) { instance_double(RBACApiClient::GroupOut, :name => 'group1', :uuid => "123") }
+  let(:group2) { instance_double(RBACApiClient::GroupOut, :name => 'group2', :uuid => "12345") }
+  let(:group3) { instance_double(RBACApiClient::GroupOut, :name => 'group3', :uuid => "45665") }
+  let(:role1) { instance_double(RBACApiClient::RoleOut, :name => "#{app_name}-#{resource}-#{resource_id1}-group-#{group1.uuid}", :uuid => "67899") }
+  let(:role2) { instance_double(RBACApiClient::RoleOut, :name => "#{app_name}-#{resource}-#{resource_id2}-group-#{group1.uuid}", :uuid => "55555") }
+  let(:role1_detail) { instance_double(RBACApiClient::RoleWithAccess, :name => role1.name, :uuid => role1.uuid, :access => [access1]) }
+  let(:role2_detail) { instance_double(RBACApiClient::RoleWithAccess, :name => role2.name, :uuid => role2.uuid, :access => []) }
+  let(:role1_detail_updated) { instance_double(RBACApiClient::RoleWithAccess, :name => role1.name, :uuid => role1.uuid, :access => []) }
   let(:groups) { [group1, group2, group3] }
   let(:roles) { [role1] }
-  let(:policies) { [double(:group => group1, :roles => roles)] }
-  let(:resource_def1) { double(:attribute_filter => double(:key => 'id', :operation => 'equal', :value => resource_id1)) }
-  let(:resource_def2) { double(:attribute_filter => double(:key => 'id', :operation => 'equal', :value => resource_id2)) }
-  let(:resource_def3) { double(:attribute_filter => double(:key => 'id', :operation => 'equal', :value => resource_id3)) }
-  let(:access1) { double(:permission => "#{app_name}:#{resource}:read", :resource_definitions => [resource_def1]) }
-  let(:access2) { double(:permission => "#{app_name}:#{resource}:write", :resource_definitions => [resource_def2]) }
-  let(:access3) { double(:permission => "#{app_name}:#{resource}:order", :resource_definitions => []) }
+  let(:policies) { [instance_double(RBACApiClient::PolicyIn, :group => group1, :roles => roles)] }
+  let(:filter1) { instance_double(RBACApiClient::ResourceDefinitionFilter, :key => 'id', :operation => 'equal', :value => resource_id1) }
+  let(:resource_def1) { instance_double(RBACApiClient::ResourceDefinition, :attribute_filter => filter1) }
+  let(:filter2) { instance_double(RBACApiClient::ResourceDefinitionFilter, :key => 'id', :operation => 'equal', :value => resource_id2) }
+  let(:resource_def2) { instance_double(RBACApiClient::ResourceDefinition, :attribute_filter => filter2) }
+  let(:filter3) { instance_double(RBACApiClient::ResourceDefinitionFilter, :key => 'id', :operation => 'equal', :value => resource_id3) }
+  let(:resource_def3) { instance_double(RBACApiClient::ResourceDefinition, :attribute_filter => filter3) }
+  let(:filter4) { instance_double(RBACApiClient::ResourceDefinitionFilter, :key => 'id', :operation => 'equal', :value => '*') }
+  let(:resource_def4) { instance_double(RBACApiClient::ResourceDefinition, :attribute_filter => filter4) }
+  let(:access1) { instance_double(RBACApiClient::Access, :permission => "#{app_name}:#{resource}:read", :resource_definitions => [resource_def1]) }
+  let(:access2) { instance_double(RBACApiClient::Access, :permission => "#{app_name}:#{resource}:write", :resource_definitions => [resource_def2]) }
+  let(:access3) { instance_double(RBACApiClient::Access, :permission => "#{app_name}:#{resource}:order", :resource_definitions => []) }
+  let(:admin_access) { instance_double(RBACApiClient::Access, :permission => "#{app_name}:#{resource}:read", :resource_definitions => [resource_def4]) }
   let(:group_uuids) { [group1.uuid, group2.uuid, group3.uuid] }
   let(:api_instance) { double }
   let(:rs_class) { class_double("RBAC::Service").as_stubbed_const(:transfer_nested_constants => true) }
   let(:current_user) { '{{username}}' }
   let(:id_value) { '*' }
-  let(:owner_resource_def) { double(:attribute_filter => double(:key => 'owner', :operation => 'equal', :value => current_user)) }
-  let(:id_resource_def) { double(:attribute_filter => double(:key => 'id', :operation => 'equal', :value => id_value)) }
+  let(:owner_filter) { instance_double(RBACApiClient::ResourceDefinitionFilter, :key => 'owner', :operation => 'equal', :value => current_user) }
+  let(:owner_resource_def) { instance_double(RBACApiClient::ResourceDefinition, :attribute_filter => owner_filter) }
+  let(:id_filter) { instance_double(RBACApiClient::ResourceDefinitionFilter, :key => 'id', :operation => 'equal', :value => id_value) }
+  let(:id_resource_def) { instance_double(RBACApiClient::ResourceDefinition, :attribute_filter => id_filter) }
   let(:owner_resource) { 'orders' }
   let(:owner_permission) { "#{app_name}:#{owner_resource}:read" }
-  let(:owner_access) { double(:permission => owner_permission, :resource_definitions => [owner_resource_def]) }
-  let(:all_access) { double(:permission => owner_permission, :resource_definitions => [id_resource_def]) }
+  let(:owner_access) { instance_double(RBACApiClient::Access, :permission => owner_permission, :resource_definitions => [owner_resource_def]) }
+  let(:all_access) { instance_double(RBACApiClient::Access, :permission => owner_permission, :resource_definitions => [id_resource_def]) }
 end
