db82e2d08deaff7551a2baed3d4faa382e7b3671
Jacob Lindgren <jlindgre@redhat.com>
2019-04-30 16:43:33 -0500

    Add requirement on service_offering_ref to throw 422 instead of 404

--- app/controllers/api/v1x0/portfolio_items_controller.rb
+++ app/controllers/api/v1x0/portfolio_items_controller.rb
@@ -39,7 +39,7 @@ module Api
       private
 
       def portfolio_item_params
-        params.permit(:service_offering_ref, :workflow_ref)
+        params.permit(:service_offering_ref, :workflow_ref).require(:service_offering_ref)
       end
 
       def portfolio_item_patch_params
