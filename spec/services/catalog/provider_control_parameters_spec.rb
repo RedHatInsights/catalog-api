bb6100ddf67b79b75826db2ecf07e9ea5a6de6d3
Greg McCullough <gmccullo@redhat.com>
2019-05-05 09:15:45 -0400

    Create different project names.
    The tests are checking which name came first, but both names were the same.
    Also, making the names more OpenShift-like by changing spaces to "-".

--- spec/services/catalog/provider_control_parameters_spec.rb
+++ spec/services/catalog/provider_control_parameters_spec.rb
@@ -7,13 +7,15 @@ describe Catalog::ProviderControlParameters do
   let(:params) { portfolio_item.id }
   let(:provider_control_parameters) { described_class.new(params) }
   let(:portfolio_item) { create(:portfolio_item, :service_offering_source_ref => source_id) }
+  let(:project1_name) { 'project-one' }
+  let(:project2_name) { 'project-two' }
   let(:project1) do
-    TopologicalInventoryApiClient::ContainerProject.new('name'      => "project one",
-                                                        'source_id' => "1")
+    TopologicalInventoryApiClient::ContainerProject.new('name'      => project1_name,
+                                                        'source_id' => "2")
   end
   let(:project2) do
-    TopologicalInventoryApiClient::ContainerProject.new('name'      => "project one",
-                                                        'source_id' => "2")
+    TopologicalInventoryApiClient::ContainerProject.new('name'      => project2_name,
+                                                        'source_id' => "3")
   end
 
   let(:ti_class) { class_double("TopologicalInventory").as_stubbed_const(:transfer_nested_constants => true) }
@@ -27,7 +29,7 @@ describe Catalog::ProviderControlParameters do
     expect(api_instance).to receive(:list_source_container_projects).and_return(result)
 
     data = provider_control_parameters.process.data
-    expect(data['properties']['namespace']['enum'].first).to eq("project one")
+    expect(data['properties']['namespace']['enum'].first).to eq(project1_name)
   end
 
   context "invalid portfolio item" do
