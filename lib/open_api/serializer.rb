6eb98c1f70768b8729968ef4bf33ce3530699d6b
Drew Bomhof <dbomhof@redhat.com>
2019-05-17 17:10:55 -0400

    Modify openapi.json with dates Using temporary version of the new serializer Which validates attributes that are exposed through openapi.json We were testing for some date entries that didn't exist in the spec

--- lib/open_api/serializer.rb
+++ lib/open_api/serializer.rb
@@ -1,14 +1,29 @@
 module OpenApi
   module Serializer
     def as_json(arg = {})
-      return super unless kind_of?(ApplicationRecord)
-
-      super.tap do |hash|
-        hash.each_key do |attr_key|
-          hash[attr_key] = hash[attr_key].iso8601 if hash[attr_key].kind_of?(Time)
-          hash[attr_key] = hash[attr_key].to_s if attr_key.ends_with?("_id") || attr_key == "id"
-        end
+      previous = super
+      encrypted_columns_set = (self.class.try(:encrypted_columns) || []).to_set
+      encryption_filtered = previous.except(*encrypted_columns_set)
+      url = ManageIQ::API::Common::Request.current.instance_variable_get(:@original_url) || nil
+      base_path = url ? URI.parse(url).path : arg[:prefixes].first
+      version = api_version_from_prefix(base_path)
+      return encryption_filtered unless arg.key?(:prefixes) || version
+      schema  = Api::Docs[version].definitions[self.class.name]
+      attrs   = encryption_filtered.slice(*schema["properties"].keys)
+      schema["properties"].keys.each do |name|
+        next if attrs[name].nil?
+        attrs[name] = attrs[name].iso8601 if attrs[name].kind_of?(Time)
+        attrs[name] = attrs[name].to_s if name.ends_with?("_id") || name == "id"
+        attrs[name] = self.public_send(name) if !attrs.key?(name) && !encrypted_columns_set.include?(name)
       end
+      attrs.compact
+    end
+
+    def api_version_from_prefix(prefix)
+      return unless prefix
+      /\/?\w+\/v(?<major>\d+)[x\.]?(?<minor>\d+)?\// =~ prefix
+      [major, minor].compact.join(".")
     end
   end
 end
+
