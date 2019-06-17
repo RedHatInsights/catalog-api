0f33e7eeeb4d19bcf045a7c72f35d5e4eba1b1e9
Erik Clarizio <eclarizio@gmail.com>
2019-06-17 14:20:04 -0700

    Adjust specs and logic for notify class that handles delegation

--- spec/controllers/internal/v0x0/notify_controller_spec.rb
+++ spec/controllers/internal/v0x0/notify_controller_spec.rb
@@ -17,7 +17,16 @@ describe Internal::V0x0::NotifyController, :type => :request do
     end
 
     context "when the class provided is supported" do
+      let(:klass) { "order_item" }
+      let(:tenant) { create(:tenant) }
+      let(:order) { create(:order, :tenant_id => tenant.id) }
+      let(:portfolio_item) { create(:portfolio_item, :service_offering_ref => "123", :tenant_id => tenant.id) }
+      let!(:order_item) { create(:order_item, :order_id => order.id, :portfolio_item_id => portfolio_item.id, :tenant_id => tenant.id) }
+
       it "returns the object" do
+        post "/internal/v0.0/notify/#{klass}/#{order_item.id}", :headers => default_headers, :params => {:payload => {:decision => "test"}.to_json}
+
+        expect(response.body).to eq(order_item)
       end
     end
   end
