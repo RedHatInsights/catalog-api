f39c54b17229b0b88db7c4c77fbc55b3f89c19bc
Jacob Lindgren <jlindgre@redhat.com>
2019-07-02 15:22:51 -0500

    Add settings per tenant

--- app/models/tenant.rb
+++ app/models/tenant.rb
@@ -1,3 +1,13 @@
 class Tenant < ApplicationRecord
   validates :external_tenant, :uniqueness => true, :presence => true
+
+  after_initialize :setup_settings, :unless => Proc.new { settings.nil? }
+
+  def setup_settings
+    settings.keys.each do |key|
+      define_singleton_method(key) do
+        settings[key]
+      end
+    end
+  end
 end
