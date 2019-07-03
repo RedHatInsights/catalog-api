b8ee03d52ae21f16100e335a1a4db0757f9d312d
Jacob Lindgren <jlindgre@redhat.com>
2019-07-03 15:21:56 -0500

    add rspec tests for tenant settings functionality

--- app/models/tenant.rb
+++ app/models/tenant.rb
@@ -1,7 +1,23 @@
 class Tenant < ApplicationRecord
   validates :external_tenant, :uniqueness => true, :presence => true
 
-  after_initialize :setup_settings, :unless => Proc.new { settings.nil? }
+  after_initialize :setup_settings, :unless => proc { settings.nil? }
+
+  def add_settings(setting)
+    settings.merge!(setting)
+    save!
+  end
+
+  def update_setting(setting)
+    add_settings(setting)
+  end
+
+  def delete_setting(setting)
+    settings.delete(setting.to_s)
+    save!
+  end
+
+  private
 
   def setup_settings
     settings.keys.each do |key|
---
+++ spec/models/tenant_spec.rb
@@ -0,0 +1,53 @@
+describe Tenant, :type => :model do
+  let(:tenant) do
+    create(:tenant,
+           :settings => {
+             :icon             => "<svg rel='stylesheet'>image</svg>",
+             :default_workflow => "1"
+           })
+  end
+  let(:retreived_tenant) { Tenant.find(tenant.id) }
+
+  describe "#setup_settings" do
+    context "when retreiving from the db" do
+      it "populates the keys as methods" do
+        expect(retreived_tenant.icon).to eq tenant.settings["icon"]
+        expect(retreived_tenant.default_workflow).to eq tenant.settings["default_workflow"]
+      end
+    end
+
+    context "when manually adding a new setting and retreiving the key" do
+      before do
+        tenant.update!(:settings => tenant.settings.merge!(:a_setting => "nerp"))
+      end
+
+      it "has the new setting on the hash" do
+        expect(retreived_tenant.a_setting).to eq "nerp"
+      end
+    end
+  end
+
+  describe "#add_settings" do
+    before { tenant.add_settings(:telephone => "555-867-5309") }
+
+    it "adds the specified setting" do
+      expect(retreived_tenant.telephone).to eq "555-867-5309"
+    end
+  end
+
+  describe "#update_setting" do
+    before { tenant.update_setting(:default_workflow => "2") }
+
+    it "updates the setting" do
+      expect(retreived_tenant.default_workflow).to eq "2"
+    end
+  end
+
+  describe "#delete_setting" do
+    before { tenant.delete_setting(:icon) }
+
+    it "deletes the setting" do
+      expect { retreived_tenant.icon }.to raise_exception(NoMethodError)
+    end
+  end
+end
