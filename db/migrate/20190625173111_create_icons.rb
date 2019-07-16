e948bcde73c18f89970b69b505ce24200e22e5b7
Jacob Lindgren <jlindgre@redhat.com>
2019-07-16 10:13:12 -0500

    edit the icon migration to use a rake task to move the icon data to the new table

--- db/migrate/20190625173111_create_icons.rb
+++ db/migrate/20190625173111_create_icons.rb
@@ -11,20 +11,16 @@ class CreateIcons < ActiveRecord::Migration[5.2]
       t.timestamps
     end
 
-    PortfolioItem.all.each do |item|
-      ManageIQ::API::Common::Request.with_request(dummy_request) do
-        icon = TopologicalInventory.call { |api| api.show_service_offering_icon(item.service_offering_icon_ref) }
+    puts <<~EOMESSAGE
+      XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+      After this migration, one needs to run
+      `rake db:migrate:icons EMAIL=my_real_email@redhat.com`
+      to actually move all of the icon references to the new table.
 
-        Icon.create!(
-          :data              => icon.data,
-          :source_ref        => icon.source_ref,
-          :source_id         => icon.source_id,
-          :portfolio_item_id => item.id
-        )
-      end
-    end
-
-    remove_column :portfolio_items, :service_offering_icon_ref, :string
+      Until this is done, the icon endpoints will be broken since the
+      references aren't correct.
+      XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+    EOMESSAGE
   end
 
   def down
@@ -32,55 +28,9 @@ class CreateIcons < ActiveRecord::Migration[5.2]
     add_column :portfolio_items, :service_offering_icon_ref, :string
   end
 
-  private
-
-  def dummy_request
-    {
-      :headers      => {
-        'x-rh-identity'            => encoded_user_hash,
-        'x-rh-insights-request-id' => "rails_migration"
-      },
-      :original_url => "catalog-api"
-    }
-  end
-
-  def encoded_user_hash
-    user_hash = {
-      "entitlements" => {
-        "hybrid_cloud"     => {
-          "is_entitled" => true
-        },
-        "insights"         => {
-          "is_entitled" => true
-        },
-        "openshift"        => {
-          "is_entitled" => true
-        },
-        "smart_management" => {
-          "is_entitled" => true
-        }
-      },
-      "identity"     => {
-        "account_number" => "0369233",
-        "type"           => "User",
-        "user"           => {
-          "username"     => "jdoe",
-          "email"        => "jdoe@acme.com",
-          "first_name"   => "John",
-          "last_name"    => "Doe",
-          "is_active"    => true,
-          "is_org_admin" => true, # set the org_admin flag since we're going to need to access everything
-          "is_internal"  => false,
-          "locale"       => "en_US"
-        },
-        "internal"       => {
-          "org_id"    => "3340851",
-          "auth_type" => "basic-auth",
-          "auth_time" => 6300
-        }
-      }
-    }
-
-    Base64.strict_encode64(user_hash.to_json)
+  # Only gets called from `rake icons:migrate`, which needs to be run after this migration to move all of the
+  # icons from a reference on PortfolioItem to the new Icon model
+  def finalize_migration
+    remove_column :portfolio_items, :service_offering_icon_ref, :string
   end
 end
