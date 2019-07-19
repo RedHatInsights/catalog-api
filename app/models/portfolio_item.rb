dc94f0211b968dae77b0f037e0212c9b2cc0426d
Jacob Lindgren <jlindgre@redhat.com>
2019-07-19 11:26:45 -0500

    add boolean validation on PortfolioItem favorite field

--- app/models/portfolio_item.rb
+++ app/models/portfolio_item.rb
@@ -8,6 +8,7 @@ class PortfolioItem < ApplicationRecord
   has_many :icons, :dependent => :destroy
   belongs_to :portfolio
   validates :service_offering_ref, :presence => true
+  validates :favorite_before_type_cast, :format => { :with => /\A(true|false)\z/i }, :allow_blank => true
 
   def resolved_workflow_refs
     # TODO: Add lookup to platform workflow ref
