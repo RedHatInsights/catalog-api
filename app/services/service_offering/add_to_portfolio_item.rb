dc070a7a3ef43d6ef9e71716e5da39934f65b330
Greg McCullough <gmccullo@redhat.com>
2019-07-27 10:31:38 -0400

    Only set long_description when provided as part of the source_offering

--- app/services/service_offering/add_to_portfolio_item.rb
+++ app/services/service_offering/add_to_portfolio_item.rb
@@ -41,8 +41,6 @@ module ServiceOffering
     # If certain fields are empty populate them from the other fields that we do have.
     def populate_missing_fields
       @params[:service_offering_source_ref] = @service_offering.source_id
-      # Fill up empty fields if they're empty with fields we already have, this is really easy with the ||= and subsequent || operators
-      @params[:long_description] ||= @params[:description] || @params[:display_name]
       @params[:display_name] ||= @params[:name]
       @params
     end
