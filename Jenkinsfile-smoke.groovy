25db0922db9cedd9719e8f0722e860ef7b3fe5d0
brahmani <brahmani@redhat.com>
2019-08-06 18:13:50 +0300

    Update Jenkinsfile-smoke.groovy file with minion deployment

--- Jenkinsfile-smoke.groovy
+++ Jenkinsfile-smoke.groovy
@@ -7,8 +7,8 @@
 
 if (env.CHANGE_ID) {
     runSmokeTest (
-        ocDeployerBuilderPath: "catalog/catalog-api",
-        ocDeployerComponentPath: "catalog/catalog-api",
+        ocDeployerBuilderPath: "catalog/catalog-api,catalog/catalog-api-minion",
+        ocDeployerComponentPath: "catalog/catalog-api,catalog/catalog-api-minion-approval,catalog/catalog-api-minion-order",
         ocDeployerServiceSets: "catalog,topological-inventory,sources,approval,platform-mq",
         iqePlugins: ["iqe-self-service-portal-plugin"],
         pytestMarker: "catalog_api_smoke",
