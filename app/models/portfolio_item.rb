4f0f26a404fde6c76353a0616bc0b9f578095da9
Drew Bomhof <dbomhof@redhat.com>
2019-09-10 13:34:07 -0400

    validate name, display_name

--- app/models/portfolio_item.rb
+++ app/models/portfolio_item.rb
@@ -7,7 +7,7 @@ class PortfolioItem < ApplicationRecord
 
   has_many :icons, :dependent => :destroy
   belongs_to :portfolio, :optional => true
-  validates :service_offering_ref, :presence => true
+  validates :service_offering_ref, :name, :display_name, :presence => true
 
   def resolved_workflow_refs
     # TODO: Add lookup to platform workflow ref
