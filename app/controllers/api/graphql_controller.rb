c62d17394d722daddb64bce5364c2ff0eb277afd
Brandon Dunne <brandondunne@hotmail.com>
2019-09-20 16:28:25 -0400

    Don't attempt to validate the post body or primary collection id

--- app/controllers/api/graphql_controller.rb
+++ app/controllers/api/graphql_controller.rb
@@ -2,6 +2,9 @@ require "manageiq/api/common/graphql"
 
 module Api
   class GraphqlController < ApplicationController
+    skip_before_action :validate_request
+    skip_before_action :validate_primary_collection_id
+
     def query
       graphql_api_schema = ::ManageIQ::API::Common::GraphQL::Generator.init_schema(request)
       variables = ::ManageIQ::API::Common::GraphQL.ensure_hash(params[:variables])
