d18bac5cc32457f69b81bbf3272c3233b296a6cc
Madhu Kanoor <mkanoor@redhat.com>
2020-01-17 18:35:14 -0500

    Fixed a bug in the root controller spec
    There was a leading / causing the specs to fail for
    fetching the openapi.json when no headers were provided
    Also removed the log messages added during testing in
    PR # https://github.com/RedHatInsights/catalog-api/pull/558

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,16 +14,12 @@ class ApplicationController < ActionController::API
   private
 
   def with_current_request
-    logger.info("Handling incoming request")
     Insights::API::Common::Request.with_request(request) do |current|
-      logger.info(Insights::API::Common::Request.current_forwardable.to_s)
       if current.required_auth?
-        logger.info("Running with tenant")
         raise Insights::API::Common::EntitlementError, "User not Entitled" unless check_entitled(current.entitlement)
 
         ActsAsTenant.with_tenant(current_tenant(current.user)) { yield }
       else
-        logger.info("Running without tenant")
         ActsAsTenant.without_tenant { yield }
       end
     end
