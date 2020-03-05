2fc8e294991d87504f812bfd05a127fa19c8441b
Drew Bomhof <dbomhof@redhat.com>
2020-03-05 14:14:09 -0500

    Write the spec/support helper and write to the spec_helper file

--- lib/api/tools/add.rb
+++ lib/api/tools/add.rb
@@ -3,9 +3,28 @@ module Api
   module Tools
     class Add < Versioning
       def apply
-        %w[dirs files new_routes draw_file server_names].each do |apply|
+        %w[dirs files new_routes draw_file server_names spec_helpers spec_helper].each do |apply|
           self.send(apply)
         end
+      end
+
+      private
+
+      def spec_helper
+        file_name = "spec/spec_helper.rb"
+        text = File.read(file_name)
+        text = text.gsub("config.include V#{@previous}Helper, :type => :v#{@previous}", "config.include V#{@previous}Helper, :type => :v#{@previous}\n  config.include V#{@version}Helper, :type => :v#{@version}")
+
+        write(file_name, text)
+      end
+
+      def spec_helpers
+        file_name = "spec/support/v#{@version}_helper.rb"
+        text = File.read(file_name)
+        text = text.gsub("#{@dot_prev}","#{@dot_new}")
+        text = text.gsub("#{@previous}","#{@version}")
+
+        write(file_name, text)
       end
 
       def server_names
@@ -35,18 +54,17 @@ module Api
       end
 
       def dirs
-        # build new dirs
         %w[app/controllers/api spec/requests/api].each do |dir|
           path = dir.match('requests') ? "#{dir}/v#{@dot_new}" : "#{dir}/v#{@version}"
-          raise StandardError.new("Path already exists") if check_existing?(path)
+          raise StandardError.new("Path: #{path} already exists") if check_existing?(path)
           FileUtils.mkdir(path)
         end
       end
 
       def files
-        prev = ["app/controllers/api/v#{@previous}.rb", "config/routes/v#{@previous}.rb", "public/doc/openapi-3-v#{@dot_prev}.json"]
-        ["app/controllers/api/v#{@version}.rb", "config/routes/v#{@version}.rb", "public/doc/openapi-3-v#{@dot_new}.json"].each_with_index do |file, index|
-          raise StandardError.new("File already exists") if check_existing?(file)
+        prev = ["app/controllers/api/v#{@previous}.rb", "config/routes/v#{@previous}.rb", "public/doc/openapi-3-v#{@dot_prev}.json", "spec/support/v#{@previous}_helper.rb"]
+        ["app/controllers/api/v#{@version}.rb", "config/routes/v#{@version}.rb", "public/doc/openapi-3-v#{@dot_new}.json", "spec/support/v#{@version}_helper.rb"].each_with_index do |file, index|
+          raise StandardError.new("File: #{file} already exists") if check_existing?(file)
           FileUtils.cp(prev[index], file)
         end
       end
--- lib/api/tools/remove.rb
+++ lib/api/tools/remove.rb
@@ -3,16 +3,26 @@ module Api
   module Tools
     class Remove < Versioning
       def apply
-        %w[dirs files routes].each do |apply|
+        %w[dirs files routes spec_helper].each do |apply|
           send(apply)
         end
       end
+
+      private
 
       def routes
         file_name = "config/routes.rb"
         text = File.read(file_name)
         text = text.gsub("#{@dot_new}", "#{@dot_prev}")
         text = text.gsub("draw(:v#{@version})", '')
+
+        write(file_name, text)
+      end
+
+      def spec_helper
+        file_name = "spec/spec_helper.rb"
+        text = File.read(file_name)
+        text = text.gsub("config.include V#{@version}Helper, :type => :v#{@version}", '')
 
         write(file_name, text)
       end
@@ -25,7 +35,7 @@ module Api
       end
 
       def files
-        ["app/controllers/api/v#{@version}.rb", "config/routes/v#{@version}.rb", "public/doc/openapi-3-v#{@dot_new}.json"].each_with_index do |file, index|
+        ["app/controllers/api/v#{@version}.rb", "config/routes/v#{@version}.rb", "public/doc/openapi-3-v#{@dot_new}.json", "spec/support/v#{@version}_helper.rb"].each_with_index do |file, index|
           FileUtils.rm(file)
         end
       end
