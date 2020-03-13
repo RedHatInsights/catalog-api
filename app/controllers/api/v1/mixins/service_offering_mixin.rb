8c8917dcde61e5884ca9c604b277e2678d0de21e
Erik Clarizio <eclarizio@gmail.com>
2020-03-13 14:31:19 -0700

    Add failure state and progress messages for archived service offerings

--- app/controllers/api/v1/mixins/service_offering_mixin.rb
+++ app/controllers/api/v1/mixins/service_offering_mixin.rb
@@ -4,13 +4,22 @@ module Api
       module ServiceOfferingMixin
         private
 
-        def service_offering_check(order)
-          service_offering_service = Catalog::ServiceOffering.new(order).process
+        def service_offering_check
+          service_offering_service = Catalog::ServiceOffering.new(@order).process
           if service_offering_service.archived
-            raise Catalog::ServiceOfferingArchived, "Service offering for order #{order.id} has been archived and can no longer be ordered"
-          end
+            @order.order_items.each do |order_item|
+              order_item.update!(:completed_at => DateTime.now, :state => "Failed")
+              order_item.update_message("error", archived_error_message)
+            end
 
-          true
+            @order.update!(:state => "Failed")
+
+            raise Catalog::ServiceOfferingArchived, archived_error_message
+          end
+        end
+
+        def archived_error_message
+          "Order Failed: Service offering for order #{@order.id} has been archived and can no longer be ordered"
         end
       end
     end
