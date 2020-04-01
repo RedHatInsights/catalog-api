17639341d5a76772517ffc2cc291a122a64da345
Erik Clarizio <eclarizio@gmail.com>
2020-04-01 09:45:28 -0700

    Increase spec coverage for policy scopes

--- spec/policies/portfolio_policy_scope_spec.rb
+++ spec/policies/portfolio_policy_scope_spec.rb
@@ -1,14 +1,12 @@
-describe PortfolioItemPolicy::Scope, :type => [:service] do
+describe PortfolioPolicy::Scope, :type => [:service] do
   let(:user_context) { instance_double(UserContext, :group_uuids => ["123-456"], :access => catalog_access) }
   let(:catalog_access) { instance_double(Insights::API::Common::RBAC::Access, :scopes => scopes) }
-  let(:scope) { PortfolioItem }
+  let(:scope) { Portfolio }
   let(:rbac_access) { instance_double(Catalog::RBAC::Access) }
   let(:subject) { described_class.new(user_context, scope) }
 
   describe "#resolve" do
     let(:portfolio) { create(:portfolio) }
-    let!(:portfolio_item1) { create(:portfolio_item, :portfolio => portfolio) }
-    let!(:portfolio_item2) { create(:portfolio_item) }
 
     around do |example|
       with_modified_env(:RBAC_URL => "http://rbac") do
@@ -17,14 +15,14 @@ describe PortfolioItemPolicy::Scope, :type => [:service] do
     end
 
     before do
-      allow(Catalog::RBAC::Access).to receive(:new).with(user_context, portfolio_item1).and_return(rbac_access)
+      allow(Catalog::RBAC::Access).to receive(:new).with(user_context, portfolio).and_return(rbac_access)
     end
 
     context "when the user is in the admin scope" do
       let(:scopes) { %w[admin group user] }
 
-      it "returns all of the portfolio items" do
-        expect(subject.resolve).to contain_exactly(portfolio_item1, portfolio_item2)
+      it "returns all of the portfolios" do
+        expect(subject.resolve).to contain_exactly(portfolio)
       end
     end
 
@@ -42,7 +40,7 @@ describe PortfolioItemPolicy::Scope, :type => [:service] do
         end
 
         it "returns the set limited by the correct portfolio ids" do
-          expect(subject.resolve).to eq([portfolio_item1])
+          expect(subject.resolve).to eq([portfolio])
         end
       end
 
@@ -61,7 +59,7 @@ describe PortfolioItemPolicy::Scope, :type => [:service] do
       let(:scopes) { %w[user] }
 
       it "returns the set limited by the portfolio items within the owner group" do
-        expect(subject.resolve.to_a).to eq([portfolio_item1, portfolio_item2])
+        expect(subject.resolve.to_a).to eq([portfolio])
       end
 
 
@@ -84,7 +82,7 @@ describe PortfolioItemPolicy::Scope, :type => [:service] do
       let(:scopes) { %w[test] }
 
       it "raises an error" do
-        expect { subject.resolve }.to raise_error(Catalog::NotAuthorized, "Not Authorized for portfolio_items")
+        expect { subject.resolve }.to raise_error(Catalog::NotAuthorized, "Not Authorized for portfolios")
       end
     end
   end
