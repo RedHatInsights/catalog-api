7944d658fb9b22b186151f1f4053e6410c0b35b0
Erik Clarizio <eclarizio@gmail.com>
2020-04-03 12:17:06 -0700

    Move user capabilities to a mixin and include in relevant models

---
+++ app/services/catalog/user_capabilities.rb
@@ -0,0 +1,22 @@
+module Catalog
+  module UserCapabilities
+    include Pundit
+    extend ActiveSupport::Concern
+
+    included do
+      attribute :metadata, ActiveRecord::Type::Json.new
+    end
+
+    private
+
+    def user_capabilities
+      return nil if user_context.nil?
+
+      "#{self.class}Policy".constantize.new(user_context, self).user_capabilities
+    end
+
+    def user_context
+      Thread.current[:user]
+    end
+  end
+end
