1672f72a1a8819f7da684bea1c870e5dd786bbca
Coady LaCroix <clacroix@redhat.com>
2020-04-29 13:26:09 -0700

    Truncate copied portfolio names to 64 characters

--- lib/catalog/name_adjust.rb
+++ lib/catalog/name_adjust.rb
@@ -5,16 +5,12 @@ module Catalog
     def self.create_copy_name(original_name, names)
       original_name.sub!(COPY_REGEX, '')
       names.select! { |name| name.match("#{COPY_REGEX} #{original_name}") }
-
-      if names.any?
-        num = get_index(names)
-        "Copy (#{num + 1}) of " + original_name
-      else
-        "Copy of " + original_name
-      end
+      copy_name(original_name, names).truncate(64)
     end
 
     class << self
+      private
+
       def get_index(names)
         ####
         # This chain of maps takes a match for "Copy (#) of #{name}" and returns the highest index.
@@ -30,6 +26,15 @@ module Catalog
           .compact
           .map { |match| match.gsub(/[()]/, "").to_i }
           .max || 0
+      end
+
+      def copy_name(original_name, names)
+        if names.any?
+          num = get_index(names)
+          "Copy (#{num + 1}) of " + original_name
+        else
+          "Copy of " + original_name
+        end
       end
     end
   end
--- spec/lib/catalog/name_adjust_spec.rb
+++ spec/lib/catalog/name_adjust_spec.rb
@@ -6,16 +6,39 @@ describe Catalog::NameAdjust do
     context "when passing in multiple Copy Names" do
       let(:names) { [item_name.to_s, "Copy of #{item_name}", "Copy (1) of #{item_name}"] }
 
-      it "returns the highest index in the array" do
-        expect(create_copy_name).to eq "Copy (2) of #{item_name}"
+      context "when the copied name would be greater than 64 characters" do
+        let(:item_name) { "CopiedNamesThatAreGreaterThanSixtyFourCharactersLongWillBeTruncated"}
+
+        it "returns the highest index in the array truncated to 64 characters" do
+          expect(create_copy_name).to eq "Copy (2) of #{item_name}".truncate(64)
+        end
+      end
+
+      context "when the copied name is less than 64 characters" do
+        let(:names) { [item_name.to_s, "Copy of #{item_name}", "Copy (1) of #{item_name}"] }
+
+        it "returns the highest index in the array" do
+          expect(create_copy_name).to eq "Copy (2) of #{item_name}"
+        end
       end
     end
 
     context "when there are not any other copies" do
       let(:names) { [item_name.to_s, "anotheritem"] }
 
-      it "returns the right copy of string" do
-        expect(create_copy_name).to eq "Copy of #{item_name}"
+      context "when the copied name would be greater than 64 characters" do
+        let(:item_name) { "CopiedNamesThatAreGreaterThanSixtyFourCharactersLongWillBeTruncated"}
+
+        it "returns the right copy of string truncated to 64 characters" do
+          expect(create_copy_name).to eq "Copy of #{item_name}".truncate(64)
+        end
+      end
+
+      context "when the copied name is less than 64 characters" do
+
+        it "returns the right copy of string" do
+          expect(create_copy_name).to eq "Copy of #{item_name}"
+        end
       end
     end
   end
