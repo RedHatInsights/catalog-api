8333ef5eabf057e7bbd89b7996874f5e21c1980f
Madhu Kanoor <mkanoor@redhat.com>
2020-05-01 11:04:15 -0400

    PR feedback

--- spec/requests/api/v1.1/portfolio_items_controller_spec.rb
+++ spec/requests/api/v1.1/portfolio_items_controller_spec.rb
@@ -14,8 +14,10 @@ describe "v1.1 - PortfolioItemRequests", :type => [:request, :topology, :v1x1] d
   end
 
   describe "GET /portfolio_items/:portfolio_item_id #show" do
-    before do
-      get "#{api_version}/portfolio_items/#{portfolio_item_id}", :headers => default_headers
+    subject { get "#{api_version}/portfolio_items/#{portfolio_item_id}", :headers => default_headers }
+
+    before do |example|
+      subject unless example.metadata[:subject_inside]
     end
 
     context 'the portfolio_item exists' do
@@ -42,6 +44,8 @@ describe "v1.1 - PortfolioItemRequests", :type => [:request, :topology, :v1x1] d
           "set_approval" => true
         )
       end
+
+       it_behaves_like "action that tests authorization", :show?, PortfolioItem
     end
 
     context 'the portfolio_item does not exist' do
