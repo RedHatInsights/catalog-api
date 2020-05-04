acf3439879aaff59fbe4cc48f70bfd779f4b312b
Madhu Kanoor <mkanoor@redhat.com>
2020-05-04 16:58:48 -0400

    Check if we can retrieve the service offering and source

--- app/services/api/v1x0/catalog/portfolio_item_orderable.rb
+++ app/services/api/v1x0/catalog/portfolio_item_orderable.rb
@@ -3,45 +3,59 @@ module Api
     module Catalog
       class PortfolioItemOrderable
         attr_reader :result
+        attr_reader :messages
 
         def initialize(portfolio_item)
           @result = false
           @portfolio_item = portfolio_item
+          @messages = []
         end
 
         def process
-          @result = !archived? && !survey_changed? && source_available?
+          fetch_source
+          fetch_service_offering
+          @result = @service_offering && @source && !archived? && !survey_changed? && source_available?
           self
         end
 
         private
 
         def source_available?
-          source.availability_status == "available"
+          @source&.availability_status == "available"
         end
 
         def archived?
-          service_offering.archived_at.present?.tap do |response|
+          @service_offering.archived_at.present?.tap do |response|
+            @messages << "Service offering archived for Portfolio Item #{@portfolio_item.name}"
             Rails.logger.debug("Service offering archived for Portfolio Item #{@portfolio_item.name}")
           end
         end
 
         def survey_changed?
           ::Catalog::SurveyCompare.any_changed?(@portfolio_item.service_plans).tap do |response|
+            @messages << "Survey Changed for Portfolio Item #{@portfolio_item.name}"
             Rails.logger.debug("Survey Changed for Portfolio Item #{@portfolio_item.name}")
           end
         end
 
-        def source
-          @source ||= Sources.call do |api_instance|
+        def fetch_source
+          @source = Sources.call do |api_instance|
             api_instance.show_source(@portfolio_item.service_offering_source_ref)
           end
+        rescue StandardError => e
+          Rails.logger.error("Source could not be retrieved for Portfolio Item #{@portfolio_item.name}")
+          Rails.logger.error(e.message)
+          @messages << "Source could not be retrieved for Portfolio Item #{@portfolio_item.name}"
         end
 
-        def service_offering
-          @service_offering ||= TopologicalInventory.call do |api|
+        def fetch_service_offering
+          @service_offering = TopologicalInventory.call do |api|
             api.show_service_offering(@portfolio_item.service_offering_ref)
           end
+        rescue StandardError => e
+          Rails.logger.error("Service offering could not be retrieved for Portfolio Item #{@portfolio_item.name}")
+          Rails.logger.error(e.message)
+          @messages << "Service offering could not be retrieved for Portfolio Item #{@portfolio_item.name}"
         end
       end
     end
--- spec/services/api/v1.0/catalog/portfolio_item_orderable_spec.rb
+++ spec/services/api/v1.0/catalog/portfolio_item_orderable_spec.rb
@@ -17,49 +17,85 @@ describe Api::V1x0::Catalog::PortfolioItemOrderable, :type => [:service, :curren
   end
 
   let(:source_response) do
-      SourcesApiClient::Source.new(:name => 'the platform', :availability_status => availability_status)
-  end
-
-  before do
-    stub_request(:get, topological_url("service_offerings/#{service_offering_ref}"))
-      .to_return(:status => 200, :body => service_offering_response.to_json, :headers => default_headers)
-    stub_request(:get, sources_url("sources/#{service_offering_source_ref}"))
-      .to_return(:status => 200, :body => source_response.to_json, :headers => default_headers)
-    allow(::Catalog::SurveyCompare).to receive(:any_changed?).with(service_plans).and_return(survey_changed)
+    SourcesApiClient::Source.new(:name => 'the platform', :availability_status => availability_status)
   end
 
   describe "#process" do
-    context "when the nothing has changed without service plans" do
-      it "returns true" do
-        expect(subject.process.result).to be_truthy
+    context "no errors" do
+      before do
+        stub_request(:get, topological_url("service_offerings/#{service_offering_ref}"))
+          .to_return(:status => 200, :body => service_offering_response.to_json, :headers => default_headers)
+        stub_request(:get, sources_url("sources/#{service_offering_source_ref}"))
+          .to_return(:status => 200, :body => source_response.to_json, :headers => default_headers)
+        allow(::Catalog::SurveyCompare).to receive(:any_changed?).with(service_plans).and_return(survey_changed)
+      end
+
+      context "when the nothing has changed without service plans" do
+        it "returns true" do
+          expect(subject.process.result).to be_truthy
+        end
+      end
+
+      context "when the nothing has changed with service plans" do
+        let(:service_plans) { [create(:service_plan, :portfolio_item => portfolio_item)] }
+        it "returns true" do
+          expect(subject.process.result).to be_truthy
+        end
+      end
+
+      context "when the source is not available" do
+        let(:availability_status) { 'not available' }
+        it "returns false" do
+          expect(subject.process.result).to be_falsey
+        end
+      end
+
+      context "when the survey has changed" do
+        let(:survey_changed) { true }
+        it "returns false" do
+          expect(subject.process.result).to be_falsey
+        end
+      end
+
+      context "when the service offering has been archived" do
+        let(:archived_at) { Time.now }
+        it "returns false" do
+          expect(subject.process.result).to be_falsey
+        end
       end
     end
 
-    context "when the nothing has changed with service plans" do
-      let(:service_plans) { [create(:service_plan, :portfolio_item => portfolio_item)] }
-      it "returns true" do
-        expect(subject.process.result).to be_truthy
+    context "with errors from topology" do
+      before do
+        stub_request(:get, topological_url("service_offerings/#{service_offering_ref}"))
+          .to_raise(Catalog::TopologyError.new("Kaboom"))
+        stub_request(:get, sources_url("sources/#{service_offering_source_ref}"))
+          .to_return(:status => 200, :body => source_response.to_json, :headers => default_headers)
+      end
+
+      context "when the service offering cannot be retrieved" do
+        it "returns false" do
+          obj = subject.process
+          expect(obj.result).to be_falsey
+          expect(obj.messages[0]).to match(/Service offering could not be retrieved/)
+        end
       end
     end
 
-    context "when the source is not available" do
-      let(:availability_status) { 'not available' }
-      it "returns false" do
-        expect(subject.process.result).to be_falsey
+    context "with errors from source" do
+      before do
+        stub_request(:get, topological_url("service_offerings/#{service_offering_ref}"))
+          .to_return(:status => 200, :body => service_offering_response.to_json, :headers => default_headers)
+        stub_request(:get, sources_url("sources/#{service_offering_source_ref}"))
+          .to_raise(Catalog::SourcesError.new("Kaboom"))
       end
-    end
 
-    context "when the survey has changed" do
-      let(:survey_changed) { true }
-      it "returns false" do
-        expect(subject.process.result).to be_falsey
-      end
-    end
-
-    context "when the service offering has been archived" do
-      let(:archived_at) { Time.now }
-      it "returns false" do
-        expect(subject.process.result).to be_falsey
+      context "when the source cannot be retrieved" do
+        it "returns false" do
+          obj = subject.process
+          expect(obj.result).to be_falsey
+          expect(obj.messages[0]).to match(/Source could not be retrieved/)
+        end
       end
     end
   end
