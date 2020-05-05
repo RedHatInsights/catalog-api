f4af2f7ca676b407082bfc42661c6f0e83b61314
Madhu Kanoor <mkanoor@redhat.com>
2020-05-05 15:09:59 -0400

    Added validation for state in order and order items

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -3,6 +3,10 @@ class OrderItem < ApplicationRecord
   include Discard::Model
   include Api::V1x0::Catalog::DiscardRestore
   destroy_dependencies :progress_messages
+  attribute :state, :string, :default => 'Created'
+  validates_inclusion_of :state,
+    :in => ["Created", "Approval Pending", "Ordered", "Failed", "Completed", "Approved", "Denied", "Canceled"].freeze,
+    :message => "state %{value} is not included in the list"
 
   acts_as_tenant(:tenant)
 
