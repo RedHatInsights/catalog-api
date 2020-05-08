9e419e80925462302bca12a1b50432e30b41a784
Madhu Kanoor <mkanoor@redhat.com>
2020-05-08 13:12:37 -0400

    Pass an instance into authorize
    The Pundit authorize method allows us to pass an instance
    which is returned when the authorization is successful. This way
    we dont have to pass in a class when doing authorization for a
    create call.

--- app/controllers/api/v1x0/portfolios_controller.rb
+++ app/controllers/api/v1x0/portfolios_controller.rb
@@ -9,9 +9,10 @@ module Api
       end
 
       def create
-        authorize(Portfolio)
+        portfolio = authorize(Portfolio.new(writeable_params_for_create))
 
-        portfolio = Portfolio.create!(writeable_params_for_create)
+        portfolio.save!
+        portfolio.reload
         render :json => portfolio
       end
 
