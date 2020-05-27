56233c9ca4197b8338ed84f0c291e32f0684cd7d
Madhu Kanoor <mkanoor@redhat.com>
2020-05-26 21:59:27 -0400

    Check for the existence of the constant

--- spec/requests/status_spec.rb
+++ spec/requests/status_spec.rb
@@ -1,29 +1,5 @@
 RSpec.describe StatusController, :type => :request do
-  let(:headers) { {"CONTENT_TYPE" => "application/json"} }
-  let(:json) { JSON.parse(response.body) }
-
-  before do
-    stub_const("ENV", "BYPASS_TENANCY" => true)
-    stub_const("ENV", "DATABASE_URL" => "postgres://admin:smartvm@localhost/insights_api_common_development?pool=5")
-  end
-
-  context "when the application is healthy" do
-    it "returns a 200" do
-      get "/health", :headers => headers
-      expect(response).to have_http_status 200
-    end
-  end
-
-  context "when the application is not healthy" do
-    before do
-      allow(PG::Connection).to receive(:ping)
-        .with(ENV['DATABASE_URL'].split("?").first)
-        .and_return PG::Connection::PQPING_NO_RESPONSE
-    end
-
-    it "returns a 500" do
-      get "/health", :headers => headers
-      expect(response).to have_http_status 500
-    end
+  it "mixes in the insights status API endpoints" do
+    expect(StatusController.ancestors.include?(Insights::API::Common::Status::Api)).to be_truthy
   end
 end
