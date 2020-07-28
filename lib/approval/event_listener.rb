60be467ad730eb7ad154a84d46ff2c90c158188b
Bill Wei <bilwei@redhat.com>
2020-07-28 17:23:27 -0400

    Add kafka listener to process workflow_deleted event from approval-api
    Also port the listener in catalog-api-minion-approval inside catalog-api

---
+++ lib/approval/event_listener.rb
@@ -0,0 +1,37 @@
+module Approval
+  class EventListener < KafkaListener
+    SERVICE_NAME = "platform.approval".freeze
+    GROUP_REF = "catalog-api-approval-minion".freeze # backward compatible
+    EVENT_WORKFLOW_DELETED = 'workflow_deleted'.freeze
+
+    def initialize(messaging_client_option)
+      super(messaging_client_option, SERVICE_NAME, GROUP_REF)
+    end
+
+    private
+
+    def process_event(event)
+      Rails.logger.info("Kafka message #{event.message} received with payload: #{event.payload}")
+
+      if event.message == EVENT_WORKFLOW_DELETED
+        remove_approval_tag(event)
+      else
+        update_approval_status(event)
+      end
+    end
+
+    def remove_approval_tag(event)
+      insights_headers = event.headers.slice('x-rh-identity', 'x-rh-insights-request-id')
+      Insights::API::Common::Request.with_request(:headers => insights_headers, :original_url => nil) do |req|
+        ActsAsTenant.with_tenant(Tenant.find_by!(:external_tenant => req.tenant)) do
+          workflow_id = event.payload['workflow_id']
+          Tag.find_by(:name => 'workflows', :namespace => 'approval', :value => workflow_id.to_s)&.destroy
+        end
+      end
+    end
+
+    def update_approval_status(event)
+      Api::V1x0::Catalog::NotifyApprovalRequest.new(event.payload['request_id'], event.payload, event.message).process
+    end
+  end
+end
