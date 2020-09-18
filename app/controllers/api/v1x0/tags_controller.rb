5a59f9dee2f90929a78ba0a0aaca9171493cbb9c
hsong-rh <hsong@redhat.com>
2020-09-18 10:56:58 -0400

    refactored tag index method

--- app/controllers/api/v1x0/tags_controller.rb
+++ app/controllers/api/v1x0/tags_controller.rb
@@ -6,26 +6,39 @@ module Api
       include TaggingMixin
 
       def index
-        if params[:portfolio_id]
-          scope = Portfolio.where(:id => params.require(:portfolio_id))
-          relevant_portfolio = policy_scope(scope, :policy_scope_class => PortfolioPolicy::Scope).first
-          raise ActiveRecord::RecordNotFound unless relevant_portfolio
-          relevant_tags = relevant_portfolio.tags || Tag.none
-
-          collection(relevant_tags, :pre_authorized => true)
-        elsif params[:portfolio_item_id]
-          scope = PortfolioItem.where(:id => params.require(:portfolio_item_id))
-          relevant_portfolio_item = policy_scope(scope, :policy_scope_class => PortfolioItemPolicy::Scope).first
-          raise ActiveRecord::RecordNotFound unless relevant_portfolio_item
-          relevant_tags = relevant_portfolio_item.tags || Tag.none
-
-          collection(relevant_tags, :pre_authorized => true)
-        else
-          collection(Tag.all)
-        end
+        tag_collection
       end
 
       private
+
+      def acceptable_params
+        {
+          :portfolio_id      => Portfolio,
+          :portfolio_item_id => PortfolioItem
+        }
+      end
+
+      def tag_collection
+        matched_resource = acceptable_params.find { |resource_id, _resource_class| params[resource_id] }
+        tags = resource_tag_collection(matched_resource.last, params.require(matched_resource.first)) if matched_resource
+
+        tags || collection(Tag.all)
+      end
+
+      def resource_tag_collection(resource_klass, resource_id)
+        scope = resource_klass.where(:id => resource_id)
+        policy_scope_klass = resource_policy_scope(resource_klass)
+        relevant_resource = policy_scope(scope, :policy_scope_class => policy_scope_klass).first
+
+        raise ActiveRecord::RecordNotFound unless relevant_resource
+
+        relevant_tags = relevant_resource.tags || Tag.none
+        collection(relevant_tags, :pre_authorized => true)
+      end
+
+      def resource_policy_scope(resource_klass)
+        "#{resource_klass.name}Policy::Scope".constantize
+      end
 
       def instance_link(instance)
         endpoint = instance.class.name.underscore
--- app/controllers/api/v1x2/tags_controller.rb
+++ app/controllers/api/v1x2/tags_controller.rb
@@ -1,34 +1,14 @@
 module Api
   module V1x2
     class TagsController < Api::V1x1::TagsController
-      def index
-        if params[:portfolio_id]
-          scope = Portfolio.where(:id => params.require(:portfolio_id))
-          relevant_portfolio = policy_scope(scope, :policy_scope_class => PortfolioPolicy::Scope).first
-          raise ActiveRecord::RecordNotFound unless relevant_portfolio
+      private
 
-          relevant_tags = relevant_portfolio.tags || Tag.none
-
-          collection(relevant_tags, :pre_authorized => true)
-        elsif params[:portfolio_item_id]
-          scope = PortfolioItem.where(:id => params.require(:portfolio_item_id))
-          relevant_portfolio_item = policy_scope(scope, :policy_scope_class => PortfolioItemPolicy::Scope).first
-          raise ActiveRecord::RecordNotFound unless relevant_portfolio_item
-
-          relevant_tags = relevant_portfolio_item.tags || Tag.none
-
-          collection(relevant_tags, :pre_authorized => true)
-        elsif params[:order_process_id]
-          scope = OrderProcess.where(:id => params.require(:order_process_id))
-          relevant_order_process = policy_scope(scope, :policy_scope_class => OrderProcessPolicy::Scope).first
-          raise ActiveRecord::RecordNotFound unless relevant_order_process
-
-          relevant_tags = relevant_order_process.tags || Tag.none
-
-          collection(relevant_tags, :pre_authorized => true)
-        else
-          collection(Tag.all)
-        end
+      def acceptable_params
+        {
+          :portfolio_id      => Portfolio,
+          :portfolio_item_id => PortfolioItem,
+          :order_process_id  => OrderProcess
+        }
       end
     end
   end
