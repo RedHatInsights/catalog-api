8352918f3e84f86956a31113642d2eae32115258
Bill Wei <bilwei@redhat.com>
2020-09-25 12:12:04 -0400

    Extract SubmitOrderItem service from SubmitNextOrderItem

--- lib/catalog/submit_next_order_item.rb
+++ lib/catalog/submit_next_order_item.rb
@@ -1,7 +1,5 @@
 module Catalog
   class SubmitNextOrderItem
-    include SourceMixin
-
     attr_reader :order
 
     def initialize(order_id)
@@ -15,7 +13,7 @@ module Catalog
       order_item = @order.order_items.find(&:can_order?)
       return self unless order_item
 
-      submit_order_item(order_item)
+      Catalog::SubmitOrderItem.new(order_item).process
 
       @order.update(:state => 'Ordered', :order_request_sent_at => Time.now.utc) unless @order.state == 'Ordered'
 
@@ -24,42 +22,6 @@ module Catalog
     rescue => e
       Rails.logger.error("Error Submitting Order #{@order_id}: #{e.message}")
       raise
-    end
-
-    private
-
-    def validate_before_submit(item)
-      raise ::Catalog::NotAuthorized unless valid_source?(item.portfolio_item.service_offering_source_ref)
-
-      return unless Catalog::SurveyCompare.any_changed?(item.portfolio_item.service_plans)
-
-      item.mark_failed("Order Item Failed: Base survey does not match Topology")
-      raise Catalog::InvalidSurvey, "Base survey does not match Topology"
-    end
-
-    def submit_order_item(item)
-      validate_before_submit(item)
-      TopologicalInventory::Service.call do |api_instance|
-        result = api_instance.order_service_offering(item.portfolio_item.service_offering_ref, parameters(item))
-        item.mark_ordered("Ordered", :topology_task_ref => result.task_id)
-        Rails.logger.info("OrderItem #{item.id} ordered with topology task ref #{result.task_id}")
-      end
-      Rails.logger.info("Order Item #{item.id} submitted for provisioning")
-    end
-
-    def parameters(item)
-      TopologicalInventoryApiClient::OrderParametersServiceOffering.new.tap do |obj|
-        obj.service_parameters = sanitized_parameters(item)
-        obj.provider_control_parameters = item.provider_control_parameters
-        obj.service_plan_id = item.service_plan_ref
-      end
-    end
-
-    def sanitized_parameters(item)
-      Catalog::OrderItemSanitizedParameters.new(
-        :order_item         => item,
-        :do_not_mask_values => true
-      ).process.sanitized_parameters
     end
   end
 end
