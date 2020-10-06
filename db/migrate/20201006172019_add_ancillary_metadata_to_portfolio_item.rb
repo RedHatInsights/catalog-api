2518566bf6336cc3b4808c351d4066cb6022be0d
hsong-rh <hsong@redhat.com>
2020-10-06 15:31:58 -0400

    Fixed spec failure

--- db/migrate/20201006172019_add_ancillary_metadata_to_portfolio_item.rb
+++ db/migrate/20201006172019_add_ancillary_metadata_to_portfolio_item.rb
@@ -5,7 +5,6 @@ class AddAncillaryMetadataToPortfolioItem < ActiveRecord::Migration[5.2]
 
   def down
     PortfolioItem.all.each do |portfolio_item|
-      portfolio_item.metadata = {'user_capabilities' => portfolio_item.ancillary_metadata.metadata_attributes[:user_capabilities]}
       portfolio_item.ancillary_metadata.destroy
     end
   end
