4a426063dc4ba6d5c0d78fd5e5abf2a651217f88
hsong-rh <hsong@redhat.com>
2020-10-06 14:14:59 -0400

    replaced change with up and down

--- db/migrate/20201006172019_add_ancillary_metadata_to_portfolio_item.rb
+++ db/migrate/20201006172019_add_ancillary_metadata_to_portfolio_item.rb
@@ -1,5 +1,12 @@
 class AddAncillaryMetadataToPortfolioItem < ActiveRecord::Migration[5.2]
-  def change
+  def up
     PortfolioItem.all.each(&:update_metadata)
+  end
+
+  def down
+    PortfolioItem.all.each do |portfolio_item|
+      portfolio_item.metadata = {'user_capabilities' => portfolio_item.ancillary_metadata.metadata_attributes[:user_capabilities]}
+      portfolio_item.ancillary_metadata.destroy
+    end
   end
 end
